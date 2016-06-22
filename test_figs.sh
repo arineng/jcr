ebf1630b238d17e9e15311fcf5bede46cdcbce66
Andrew Newton <andy@arin.net>
2016-06-22 15:45:46 -0400

    more development toward 07

--- test_figs.sh
+++ test_figs.sh
@@ -9,6 +9,8 @@ assert_raises "jcr -r figs/second_example2.jcr figs/second_example.json" 0
 assert_raises "jcr -r figs/second_example2.jcr -o figs/second_example_override.jcr figs/second_example2.json" 0
 assert_raises "jcr -r figs/rfc4627_example.jcr figs/rfc4627_example.json" 0
 assert_raises "jcr -r figs/rfc4627_example2.jcr figs/rfc4627_example.json" 0
+assert_raises "jcr -r figs/assignment_example.jcr" 1
+assert_raises "jcr -r figs/rule_name_ruleset_id.jcr" 1
 
 assert_end figs
 
