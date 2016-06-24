095ecc11d8b144e15eb1949ee2d1e4bc8ffecd50
Andrew Newton <andy@arin.net>
2016-06-24 17:45:20 -0400

    work on primitives

--- test_figs.sh
+++ test_figs.sh
@@ -11,6 +11,15 @@ assert_raises "jcr -r figs/rfc4627_example.jcr figs/rfc4627_example.json" 0
 assert_raises "jcr -r figs/rfc4627_example2.jcr figs/rfc4627_example.json" 0
 assert_raises "jcr -r figs/assignment_example.jcr" 1
 assert_raises "jcr -r figs/rule_name_ruleset_id.jcr" 1
+assert_raises "jcr -r figs/primitives_overview.jcr" 1
+assert_raises "jcr -r figs/primitives_boolean_and_null.jcr" 1
+assert_raises "jcr -r figs/primitives_integer_and_float.jcr" 1
+assert_raises "jcr -r figs/primitives_float_range.jcr" 1
+assert_raises "jcr -r figs/primitives_bit_integers.jcr" 1
+assert_raises "jcr -r figs/primitives_strings" 1
+assert_raises "jcr -r figs/primitives_uris" 1
+assert_raises "jcr -r figs/primitives_misc" 1
+assert_raises "jcr -r figs/primitives_binary" 1
 
 assert_end figs
 
