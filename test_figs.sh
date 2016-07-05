b1ee649301fb32c75148fc03aa88863af33274f4
Andrew Newton <andy@arin.net>
2016-07-05 15:34:19 -0400

    member specifications

--- test_figs.sh
+++ test_figs.sh
@@ -9,17 +9,18 @@ assert_raises "jcr -r figs/second_example2.jcr figs/second_example.json" 0
 assert_raises "jcr -r figs/second_example2.jcr -o figs/second_example_override.jcr figs/second_example2.json" 0
 assert_raises "jcr -r figs/rfc4627_example.jcr figs/rfc4627_example.json" 0
 assert_raises "jcr -r figs/rfc4627_example2.jcr figs/rfc4627_example.json" 0
-assert_raises "jcr -r figs/assignment_example.jcr" 1
-assert_raises "jcr -r figs/rule_name_ruleset_id.jcr" 1
-assert_raises "jcr -r figs/primitives_overview.jcr" 1
-assert_raises "jcr -r figs/primitives_boolean_and_null.jcr" 1
-assert_raises "jcr -r figs/primitives_integer_and_float.jcr" 1
-assert_raises "jcr -r figs/primitives_float_range.jcr" 1
-assert_raises "jcr -r figs/primitives_bit_integers.jcr" 1
-assert_raises "jcr -r figs/primitives_strings" 1
-assert_raises "jcr -r figs/primitives_uris" 1
-assert_raises "jcr -r figs/primitives_misc" 1
-assert_raises "jcr -r figs/primitives_binary" 1
+assert_raises "jcr --test-jcr -r figs/assignment_example.jcr" 0
+assert_raises "jcr -r figs/rule_name_ruleset_id.jcr figs/first_example.json" 1
+assert_raises "jcr --test-jcr -r figs/primitives_overview.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_boolean_and_null.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_integer_and_float.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_float_range.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_bit_integers.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_strings.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_uris.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_misc.jcr" 0
+assert_raises "jcr --test-jcr -r figs/primitives_binary.jcr" 0
+assert_raises "jcr --test-jcr -r figs/member_specifications.jcr" 0
 
 assert_end figs
 
